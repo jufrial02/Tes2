8594c326b34ef90746d16a9416584387337e21e8
jufrial02 <01jufrial@gmail.com>
2025-07-14 06:11:57 +0800

    Delete js/human_model/torso.js

--- js/human_model/torso.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js';
-
-export function createTorso(color = 0xffaaaa) {
-  const geometry = new THREE.CylinderGeometry(0.25, 0.3, 1.2, 12);
-  const material = new THREE.MeshStandardMaterial({ color });
-  const torso = new THREE.Mesh(geometry, material);
-  torso.position.y = 0.9;
-  return torso;
-}
